ab9e6b45e542b5a31c1cafd1acfba2e6b961cb53
Keenan Jones <keenanjones210@gmail.com>
2020-10-06 18:53:36 -0500

    Blog page incomplete

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,44 +1,11 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import React from 'react'
 
 const Blog = () => {
- const [blogs, setBlogs] = useState([]);
- const [featuredBlog, setFeaturedBlog] = useState([]);
-
- useEffect(() => {
-  const fetchData = async () => {
-   try{
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts`)
-    setFeaturedBlog(res.data[0]);
-   }
-   catch(err) {}
-  }
-  fetchData();
- }, []);
-
-
- useEffect(() => {
-  const fetchPosts = async () => {
-   try{
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/posts`)
-    setBlogs(res.data)
-   }
-   catch(err) {}
-  }
-  fetchPosts();
- }, []);
-
- const capitalizeFirstLetter = (word) => {
-  if(word)
-   return word.charAt(0).toUpperCase() + word.slice(1);
-  return '';
- }
-
- const getBlogs = 
  return (
-
- );
-};
+  <div>
+   Blog
+  </div>
+ )
+}
 
 export default Blog
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 const Home = () => {
  return (
-  <div className="container">
-   <div className="jumbotron mt-5">
-     <h1 className="display-4">Welcome to myBlog</h1>
-     <p className="lead">My personal blog to express my interest of various topics.</p>
-     <hr className="my-4"/>
-     <p>Click button to check out blog posts.</p>
-     <Link to="/blog" className="btn btn-primary btn-lg" role="button">Check Out Posts</Link>
-   </div>
+  <div>
+   Home
   </div>
  )
 }
